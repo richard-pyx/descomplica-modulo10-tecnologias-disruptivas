b5e3aab0352d4069cf0ad565b41f0207fd6e3e05
unknown <richardsoooouza@gmail.com>
2024-12-01 15:54:33 -0400

    Push inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/Login/index"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
